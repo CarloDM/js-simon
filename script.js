3b0ebbbaec50afa99ed1baa7ad850662f5630138
carloDM <carlo.de.mauro.work@gmail.com>
2023-03-31 14:30:00 +0200

    numeri estratti

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('ciao')
+/*
+  1. ARRAY NUMERI ESTRATTI
+  2. ARRAY NUMERI INSERITI
+  3. OUTPUT NUMERI IN HTML
+  4. SISTEMA DI VERIFICA
+ */
+
+let 
+numEstratti = [],
+numInseriti = [];
+
+
+for (let i = 0; i < 5; i++) {numGen()}
+console.log(numEstratti)
+
+
+
+
+
+
+function numGen() {
+  R = Math.floor(Math.random()*50)
+  if (numEstratti.includes(R)){
+  }else{
+    numEstratti.push(R)
+  }
+}
